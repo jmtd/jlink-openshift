08ea2ad72c2583759e9dd6f74c04a85780573d00
Jonathan Dowland <jdowland@redhat.com>
2021-10-11 16:24:52 +0100

    Remove superfluous sed
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- mkstrippeddeps.sh
+++ mkstrippeddeps.sh
@@ -7,7 +7,6 @@ test -f deps.txt
   sed -E "s/.*-> //"  | # remove src of src -> dep
   grep -v "not found" | # clear out unresolved deps
   sed "s#/.*##"       | # delete anything after a slash. in practice target/..
-  sed '/^$/d'         | # delete empty lines
   grep 'java\|jdk\.'  | # mostly removes target/, but also jdk8internals
   sort | uniq | tr '\n' ',' | tr -d "[:space:]" |
 tee stripped-deps2.txt
