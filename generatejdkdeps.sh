0bff658ed95014e58d4f69445fe49c47d81e7e6d
Jonathan Dowland <jdowland@redhat.com>
2023-03-22 16:01:09 +0000

    generatejdkdeps: don't define JAVA_HOME
    rely upon external JAVA_HOME
    
    fix path (bin sub-dir of JAVA_HOME)
    
    avoid a `cat`

--- generatejdkdeps.sh
+++ generatejdkdeps.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-JAVA_HOME="/lib/jvm/java-11-openjdk/bin"
-
-$JAVA_HOME/java --list-modules > java-modules.txt
-cat java-modules.txt | sed "s/\\@.*//" > modules.txt
+$JAVA_HOME/bin/java --list-modules > java-modules.txt
+< java-modules.txt sed "s/\\@.*//" > modules.txt
 grep -Fx -f stripped-deps.txt modules.txt | tr '\n' ',' | tr -d "[:space:]" > module-deps.txt
 echo "jdk.zipfs" >> module-deps.txt
