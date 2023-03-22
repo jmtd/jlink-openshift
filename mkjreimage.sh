0b58e4232ac4ea53643cc01ad724631754f9ebe7
Jonathan Dowland <jdowland@redhat.com>
2023-03-22 16:03:39 +0000

    mkjreimage: enable ZIP compression

--- mkjreimage.sh
+++ mkjreimage.sh
@@ -7,4 +7,5 @@ test -f $depsfile
 
 $JAVA_HOME/bin/jlink --output spring-boot-jre \
        	--add-modules (cat $depsfile) \
-	--strip-debug --no-header-files --no-man-pages
+	--strip-debug --no-header-files --no-man-pages \
+	--compress=2
