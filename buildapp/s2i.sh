b2356f99e4e68a0fa3c9442f328f4bc045766f97
Jonathan Dowland <jdowland@redhat.com>
2023-05-30 15:52:23 +0100

    WIP: adjust s2i environment for quarkus uber-jar
    We need to override the values that the quickstart provides in
    .s2i/environment, since those are tuned for the fast-jar build
    layout.
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- buildapp/s2i.sh
+++ buildapp/s2i.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 BASEIMG=${BASEIMG:=registry.access.redhat.com/ubi8/openjdk-17}
 APPSRC=${APPSRC:=https://github.com/quarkusio/quarkus-quickstarts.git}
 CONTEXTDIR=${CONTEXTDIR:=getting-started}
-rev=${rev:=2.16.4.Final}
+rev=${rev:=3.0.3.Final}
 OUTIMG=jlink-appsrc
 
 # Note: this directory needs to be owned by uid 185
@@ -13,6 +13,9 @@ WORKDIR=${WORKDIR:=$PWD/work}
 s2i build --pull-policy if-not-present --context-dir=$CONTEXTDIR -r=${rev} \
     -e S2I_DELETE_SOURCE=false \
     -e QUARKUS_PACKAGE_TYPE=uber-jar \
+    -e MAVEN_S2I_ARTIFACT_DIRS=target \
+    -e S2I_SOURCE_DEPLOYMENTS_FILTER="*.jar" \
+    -e JAVA_APP_JAR= \
     -v $WORKDIR:/tmp/src \
     $APPSRC \
     $BASEIMG \
